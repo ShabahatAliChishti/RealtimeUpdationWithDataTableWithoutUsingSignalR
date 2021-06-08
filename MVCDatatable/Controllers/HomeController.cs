2d73da3c31ebbafe203cfd7f71d6bd9932c3cc59
Shabahat Ali <shabahatalichishti@outlook.com>
2021-06-08 16:31:20 +0500

    Add project files.

---
+++ MVCDatatable/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Linq.Dynamic;
+using MVCDatatable.Models;
+
+namespace MVCDatatable.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Write action for return json data
+        [HttpPost]
+        public ActionResult LoadData()
+        {
+            //Get parameters
+
+            // get Start (paging start index) and length (page size for paging)
+            var draw = Request.Form.GetValues("draw").FirstOrDefault();
+            var start = Request.Form.GetValues("start").FirstOrDefault();
+            var length = Request.Form.GetValues("length").FirstOrDefault();
+            //Get Sort columns value
+            var sortColumn = Request.Form.GetValues("columns[" + Request.Form.GetValues("order[0][column]").FirstOrDefault() + "][name]").FirstOrDefault();
+            var sortColumnDir = Request.Form.GetValues("order[0][dir]").FirstOrDefault();
+
+            int pageSize = length != null ? Convert.ToInt32(length) : 0;
+            int skip = start != null ? Convert.ToInt32(start) : 0;
+            int totalRecords = 0;
+
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = (from a in dc.Customers select a);
+                //Sorting
+                if (!(string.IsNullOrEmpty(sortColumn)  &&  string.IsNullOrEmpty(sortColumnDir)))
+                {
+                    v = v.OrderBy(sortColumn + " " + sortColumnDir);
+                }
+
+                totalRecords = v.Count();
+                var data = v.Skip(skip).Take(pageSize).ToList();
+                return Json(new { draw = draw, recordsFiltered = totalRecords, recordsTotal = totalRecords, data = data }, JsonRequestBehavior.AllowGet);
+
+            }
+        }
+    }
+}
---
+++ MVCDatatable/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCDatatable.MvcApplication" Language="C#" %>
